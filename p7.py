41ea5040628403958bb9c865fd1227050c88a29a
samidhakurle <60541372+samidhakurle@users.noreply.github.com>
2023-07-15 01:33:55 +0530

    Update p7.py

--- p7.py
+++ p7.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+from sklearn.linear_model import LinearRegression
 r=open("regression.pkl","rb")
 regressor=pickle.load(r)
 exp=st.number_input("Experience in Years:",0,42,1)
